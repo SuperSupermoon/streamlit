99d3db80a2d4396bde065e881f406af65f951b29
supersupermoon <jhak.moon@gmail.com>
2024-07-30 17:54:33 +0900

    updates

--- cp.py
+++
@@ -1,9 +0,0 @@
-{\"ent\": \"opacities\", 
- \"sec\": \"FIND\", 
- \"sent\": \"Faint patchy peribronchial opacities in the lower lobes, right greater than left, have also minimally increased.\", 
- \"sent_idx\": 6, 
- \"cat\": \"ROF\", 
- \"status\": \"DP|minimally increased\",
- \"loc\": \"lower lobes, right greater than left\", 
- \"attr\": 
- {\"appr\": \"morph|patchy\", \"dist|peribronchial\", \"tmp\": \"worsened|minimally increased\"}},\n    
